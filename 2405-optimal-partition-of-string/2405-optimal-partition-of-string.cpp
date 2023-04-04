766c7501bf8ccdd3ebe50b2c0caa3ba23390d75e
Ambareen Azam <64471630+Ambareen09@users.noreply.github.com>
2023-04-04 19:57:04 +0530

    Time: 227 ms (59.62%), Space: 44.5 MB (56.56%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        int idx = 0;
+        int count = 0;
+        unordered_map<char, bool> mp;
+        while (idx < s.length()) {
+            if (mp.find(s[idx]) != mp.end()) {
+                count++; 
+                mp.clear();
+            }
+            mp[s[idx]] = true; 
+            idx++;
+        }
+        return count + 1;
+    }
+};
